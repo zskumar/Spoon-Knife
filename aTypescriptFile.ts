b97136017943eeefab3def6c6e4d87f45ebe4af0
Zaanjana Sreekumar <zaanjana.sreekumar@gmail.com>
2021-03-09 18:00:02 -0800

    Change typescript file

---
+++ aTypescriptFile.ts
@@ -0,0 +1,21 @@
+
+
+class PublicClass {
+    public constructor() { } // Added comment
+}
+abstract class Person {
+    constructor(name: string) {
+    }
+}
+
+interface PublicClass {
+
+}
+
+interface Person {
+
+}
+
+interface Distinct {
+
+}
